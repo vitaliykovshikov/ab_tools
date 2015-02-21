e9c106d5b47c4e9c021ef96a5fc85599af199de6
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-02-21 12:09:48 +0200

    Create setup py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='ab_tools',
+    version='0.1',
+    packages=['tools.tools'],
+    url='',
+    license='',
+    author='Dr.bleedjent',
+    author_email='dr.bleedjent',
+    description='Crossapps tools for all Avtobazar Apps',
+    # install_requires=[
+    #     "celery==3.1.17",
+    #     "django-celery==3.1.16",
+    # ]
+)
