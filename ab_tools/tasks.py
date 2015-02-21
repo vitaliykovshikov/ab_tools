fa1266ba9c144a6871c554ef6a47c30fd6a3bef7
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-02-21 14:00:14 +0200

    Add fix

--- ab_tools/tasks.py
+++ ab_tools/tasks.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from celery import task
 
-@task(queue='advert.update_task')
+@task(queue='elasticsearch.update_task')
 def update_advert_task(object_pk, content_type_pk):
     instance = ContentType.objects.get(pk=content_type_pk).get_object_for_this_type(pk=object_pk)
     print unicode(instance)
