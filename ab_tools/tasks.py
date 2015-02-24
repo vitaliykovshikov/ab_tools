76ad1cbd9ae19aa10775ddf7b5cf07c7043cbdd1
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-02-24 16:17:35 +0200

    Work on tasks

--- ab_tools/tasks.py
+++ ab_tools/tasks.py
@@ -1,4 +1,6 @@
 #encoding:utf-8
+from haystack import connections as haystack_connections
+
 from django.contrib.contenttypes.models import ContentType
 try:
     from celery.decorators import task
@@ -8,4 +10,6 @@ except ImportError:
 @task(queue='elasticsearch.update_task')
 def update_advert_task(object_pk, content_type_pk):
     instance = ContentType.objects.get(pk=content_type_pk).get_object_for_this_type(pk=object_pk)
-    print unicode(instance)
+    unified_index = haystack_connections['default'].get_unified_index()
+    index = unified_index.get_index(instance)
+    index.update_object(instance)
