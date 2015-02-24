3adc03aee27b0eaf715dd653ddb74cca3da39db6
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-02-24 15:38:38 +0200

    Update signal

--- ab_tools/signals.py
+++ ab_tools/signals.py
@@ -4,4 +4,4 @@ from tasks import update_advert_task
 
 
 def instance_update_search_index(sender, instance, **kwargs):
-    update_advert_task.delay(instance.pk, ContentType.objects.get_for_model(instance.__class__))
+    update_advert_task.delay(instance.pk, ContentType.objects.get_for_model(instance.__class__).pk)
