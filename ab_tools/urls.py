e0441d47be70625eda626f155e556966f4ca4d6f
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-03-04 12:31:29 +0200

    Add fix to urls.py

--- ab_tools/urls.py
+++ ab_tools/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, url
 from views import clean_cache_key
 
 urlpatterns = patterns('',
-    url(r'^ab_tools/cache/clean/(?P<key>.*)/$', clean_cache_key, name='ab_tools_clean_cache_key'),
+    url(r'^cache/clean/(?P<key>.*)/$', clean_cache_key, name='ab_tools_clean_cache_key'),
 )
